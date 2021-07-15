a059b0fe3668515d751016e1be3a164ec6709d99
Kolenchitssi <kolenchitssi@gmail.com>
2021-07-15 15:15:17 +0300

    feat: add task1-2

--- tasks/task1-2.js
+++ tasks/task1-2.js
@@ -1,25 +1,25 @@
 function summAdvanced(...args) {
-    let sum = 0;
-    for (let i = 0; i < args.length; i += 1) {
-        if (typeof args[i] === 'function') {
-            sum += +args[i]();
-          } else if (+args[i] && typeof args[i] !== 'boolean') { 
-            sum += +args[i];
-          }       
+  let sum = 0;
+  for (let i = 0; i < args.length; i += 1) {
+    if (typeof args[i] === 'function') {
+      sum += +args[i]();
+    } else if (+args[i] && typeof args[i] !== 'boolean') {
+      sum += +args[i];
     }
-    return sum;
   }
-  
-function getRandomNumber() {
-    return Math.random();
-  }
-  
-  function getTen() {
-    return 10;
-  }
-  
-  function getTenString() {
-    return '10';
-  }
+  return sum;
+}
 
-console.log(summAdvanced('abc', 1, '2', getTen, getTenString, getRandomNumber));
+function getRandomNumber() {
+  return Math.random();
+}
+
+function getTen() {
+  return 10;
+}
+
+function getTenString() {
+  return '10';
+}
+
+summAdvanced('abc', 1, '2', getTen, getTenString, getRandomNumber);
