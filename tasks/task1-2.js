4ea23171b3379f5a32632b3340e7785f22c58f52
Kolenchitssi <kolenchitssi@gmail.com>
2021-07-15 18:39:15 +0300

    fix: edit test1-2

--- tasks/task1-2.js
+++ tasks/task1-2.js
@@ -1,7 +1,7 @@
 function summAdvanced(...args) {
   let sum = 0;
   for (let i = 0; i < args.length; i += 1) {
-    if (typeof args[i] === 'function') {
+    if (typeof args[i] === 'function' && +args[i]()) {
       sum += +args[i]();
     } else if (+args[i] && typeof args[i] !== 'boolean') {
       sum += +args[i];
--- tasks/task1.js
+++ tasks/task1.js
@@ -14,7 +14,7 @@ summ(1, '2', '3', 5, 'abc');
 function summAdvanced(...args) {
   let sum = 0;
   for (let i = 0; i < args.length; i += 1) {
-    if (typeof args[i] === 'function') {
+    if (typeof args[i] === 'function' && +args[i]()) {
       sum += +args[i]();
     } else if (+args[i] && typeof args[i] !== 'boolean') {
       sum += +args[i];
