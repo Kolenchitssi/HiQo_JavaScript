d4906098072232077a3c989b8dd97225c8839e62
Kolenchitssi <kolenchitssi@gmail.com>
2021-07-15 13:25:13 +0300

    fix: edit task1-1

--- tasks/task1-1.js
+++ tasks/task1-1.js
@@ -2,7 +2,7 @@
 function summ(...args) {
   let sum = 0;
   for (let i = 0; i < args.length; i += 1) {
-    if (+args[i]) {
+    if (+args[i] && typeof args[i] !== 'boolean') {
       sum += +args[i];
     }
   }
