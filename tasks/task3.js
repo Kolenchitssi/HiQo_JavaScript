6f95887722fed7766bf00732d45e5a2b7f26ba6f
Kolenchits Sergy <Kolenchitssi@gmail.com>
2021-07-27 10:25:53 +0300

    feat: add task3-4 3-5

--- tasks/task3.js
+++ tasks/task3.js
@@ -8,6 +8,73 @@ function rememberResult(initialValue) {
   };
 }
 
+/* Task 2 */
+function callMaxTimes(numberOfTimes, func) {
+  let maxTimes = numberOfTimes;
+  const nameFunc = func;
+
+  return function () {
+    if (maxTimes === 0) return 'nothing happens';
+    maxTimes -= 1;
+    const result = nameFunc();
+    return result;
+  };
+}
+
+/* Task 3 */
+function partial(func, arg) {
+  const nameFunc = func;
+  const argFunc = arg;
+
+  return function (nameInput) {
+    const result = nameFunc(argFunc, nameInput);
+    return result;
+  };
+}
+
+/* Task 4 */
+function curry(fn) {
+  const len = fn.length;
+  function innerFn(n, args) {
+    return function actualInnerFn(a) {
+      if (n <= 1) {
+        return fn(...args, a);
+      }
+      return innerFn(n - 1, [...args, a]);
+    };
+  }
+  return innerFn(len, [])
+}
+
+/* Task 5 */
+function debounce(fn, timeOut) {
+  // HINT: setTimeout and clearTimeout should be used.
+  function startFunc() {
+    const timerId = setTimeout(fn, timeOut);
+    debounce.timeOutId = timerId;
+  }
+
+  clearTimeout(debounce.timeOutId);
+  startFunc();
+}
+
+// variant 2
+// let timeOutId = 0;
+
+// function debounce(fn, timeOut) {
+
+//   function startFunc() {
+//     const timerId = setTimeout(fn, timeOut);
+//     timeOutId = timerId;
+//   }
+//   clearTimeout(timeOutId);
+//   startFunc();
+// }
+
 module.exports = {
   rememberResult,
+  callMaxTimes,
+  partial,
+  curry,
+  debounce,
 };
