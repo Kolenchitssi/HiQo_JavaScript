b6c6ef713d468d4fabef7d508817557e6d677411
Kolenchits Sergy <Kolenchitssi@gmail.com>
2021-07-30 08:04:23 +0300

    feat: add task4-2

--- tasks/task4.js
+++ tasks/task4.js
@@ -5,6 +5,54 @@ function createObject(obj) {
   return obj;
 }
 
+/* Task 2 */
+function Collection(constructor) {
+  this.collection = [];
+
+  this.readAll = function () { return this.collection; };
+
+  this.add = function (...args) {
+    const newObj = new constructor(...args);
+    // frozen the object, you can't add new properties, only edit the current ones
+    this.collection.push(Object.seal(newObj));
+  };
+
+  this.get = function (func) {
+    const resultObj = {};
+    const resultIndex = this.collection.findIndex(func);
+    resultObj.item = this.collection[resultIndex];
+
+    resultObj.update = (funcUpdateItem) => {
+      funcUpdateItem(resultObj.item);
+      return resultObj;
+    };
+
+    resultObj.remove = () => {
+      this.collection.splice(resultIndex, 1);
+      return resultObj.item;
+    };
+
+    resultObj.read = () => resultObj.item;
+    return resultObj;
+  };
+
+  this.getBy = (func) => {
+    const resultObjItems = {};
+    resultObjItems.arrItems = this.collection.filter(func);
+
+    resultObjItems.update = function (funcUpdate) {
+      resultObjItems.arrItems.forEach((item, index) => {
+        funcUpdate(item, index);
+      });
+    };
+
+    resultObjItems.read = () => resultObjItems.arrItems;
+
+    return resultObjItems;
+  };
+}
+
 module.exports = {
   createObject,
+  Collection,
 };
